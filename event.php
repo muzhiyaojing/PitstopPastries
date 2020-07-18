fd7ad43412a1dc7004e869f0990faf9a29cffca1
Mu Yao <muyao0818@gmail.com>
2020-07-18 16:23:08 -0400

    Add new files

---
+++ event.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<head>
+	<title>Events</title>
+	<link type="text/css" href="css1.css" rel="stylesheet">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+				<a href="index.html"><img src="Image/logopits.gif" alt="Headline" style="width:500px;height:260px"></a>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="none"><a href="index.php">Home</a></li>
+				<li><a href="services.php">Services</a></li>
+				<li><a href="event.php">Event</a></li>
+				<li><a href="gallery.php">Cake Gallery</a></li>
+				<li><a href="contactus.php">Contact Us</a></li>
+				<li class="facebook"><a href="https://www.facebook.com/PitstopPastries/" target="_blank"><img src="Image/facebook.png" style="width:30px;height:30px"></a></li>
+			</ul>
+		</div>	
+		<div id="content">
+			<div class="upevent">
+				<h2>Upcoming Events</h2>
+				<p>5/13/2017 Michigan Bake Sale</p>
+			</div>
+			<div class="upevent">
+				<h2>Past Events</h2>
+				<p>3/15/2017 St. Patrick's Day Cupcakes for a bake sale at Henry Ford College</p>
+				<p>2/11/2017 Cakes and Subs at Redford Church of God</p>
+			</div>
+		</div>
+		<div id=footer>
+			<p class="pFooter">Copyright 2017 by Pistop Pastries. All rights reserved</p>
+		</div>
+		
+</html>
